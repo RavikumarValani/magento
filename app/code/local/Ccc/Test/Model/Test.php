4455382f969fc4f30290a7a166a5e089d114c1c3
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-13 13:00:25 +0530

    delete

--- app/code/local/Ccc/Test/Model/Test.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    class Ccc_Test_Model_Test extends Mage_Core_Model_Abstract
-    {
-        public function _construct()
-        {
-            $this->_init('test/test');
-        }
-    }
-?>
