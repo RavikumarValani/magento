ba4bd9da2343cbd07c8e5aa341c781f5fa11b65b
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-13 13:29:14 +0530

    simple setup

---
+++ app/code/local/Ccc/Test/Model/Test.php
@@ -0,0 +1,9 @@
+<?php 
+    class Ccc_Test_Model_Test extends Mage_Core_Model_Abstract
+    {
+        public function _construct()
+        {
+            $this->_init('test/test');
+        }
+    }
+?>
