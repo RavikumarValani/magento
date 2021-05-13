de5439d7d5602750fa40c6bcd58dcfdf00365e82
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-13 13:32:40 +0530

    eav setup

--- app/code/local/Ccc/Test/Model/Resource/Test/Collection.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    class Ccc_Test_Model_Resource_Test_Collection extends Mage_Core_Model_Resource_Db_Collection_Abstract
-    {
-        public function _construct()
-        {
-            $this->_init('test/test');
-        }
-    }
-?>
