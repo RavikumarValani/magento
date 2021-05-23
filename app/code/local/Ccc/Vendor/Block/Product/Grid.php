cacf2ebdab9940720fab24ed33dd0c047949c5ce
ravi14743 <71433998+ravi14743@users.noreply.github.com>
2021-05-23 20:57:17 +0530

    modifie edit action

--- app/code/local/Ccc/Vendor/Block/Product/Grid.php
+++ app/code/local/Ccc/Vendor/Block/Product/Grid.php
@@ -86,9 +86,9 @@ class Ccc_Vendor_Block_Product_Grid extends Mage_Core_Block_Template
 
         return $collection;
 	}
-	public function getEditUrl()
+	public function getEditUrl($id = null)
     {
-        return $this->getUrl('*/*/edit');
+        return $this->getUrl('*/*/edit', array('id' => $id));
     }
     public function _getSession()
     {
